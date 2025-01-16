1a13f3ebc80d70f1c528d2d1abb9c9f07668d663
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2025-01-16 11:39:14 +0000

    search section

--- src/components/search/SearchBox.jsx
+++ src/components/search/SearchBox.jsx
@@ -1,21 +1,4 @@
 
-// import React from "react";
-// import "./Search.css";
-
-// const SearchBox = ({ searchTerm, setSearchTerm }) => {
-//   return (
-//     <div className="search-box">
-//       <input
-//         type="text"
-//         placeholder="Search for movies..."
-//         value={searchTerm}
-//         onChange={(e) => setSearchTerm(e.target.value)}
-//       />
-//     </div>
-//   );
-// };
-
-// export default SearchBox;
 import React from "react";
 
 const SearchBox = ({ searchTerm, setSearchTerm }) => {
@@ -26,7 +9,7 @@ const SearchBox = ({ searchTerm, setSearchTerm }) => {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         placeholder="Search for movies..."
-        className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-400 dark:bg-gray-800 dark:text-white dark:border-gray-600"
+        className="w-full text-black p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-400 dark:bg-gray-800 dark:text-white dark:border-gray-600"
       />
     </div>
   );
